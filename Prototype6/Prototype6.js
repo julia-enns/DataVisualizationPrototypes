1ab7002a893a94772ce3a24a7299775b02f19a0b
alyssulee <43867556+alyssulee@users.noreply.github.com>
2020-11-27 16:27:54 -0700

    Initial commit

--- Prototype6/Prototype6.js
+++ Prototype6/Prototype6.js
@@ -3,79 +3,84 @@ window.onload = function(){
 };
 
 const MARGIN = {
-    "LEFT":100,
-    "RIGHT":100,
-    "TOP":100,
-    "BOTTOM":100,
+    "LEFT":50,
+    "RIGHT":50,
+    "TOP":50,
+    "BOTTOM":50,
 };
 
 //dimension of our workspace
-const   width  = 1500,
-    height = 1000;
+const   width  = 850,
+    height = 600;
 
 /**
  * This function loads the data and calls other necessary functions to create our visualization
  * @param dataPath - the path to your data file from the project's root folder
  */
 setup = function (dataPath) {
+    //Define reference for svg
+    let SVG = d3.select("#SVG_CONTAINER");
+
     //Load in  Data with D3 and call stackedBarChart
     data = d3.csv(dataPath)
         .then(function (data)
         {
             console.log(data);
-            line = new lineGraph(data);
+            line = new lineGraph(data, SVG);
         })
 
 };
 
-
-lineGraph = function (data) {
-    // set the dimensions and margins of the graph
-    var margin = {top: 20, right: 30, bottom: 30, left: 55},
-        width = 1500 - margin.left - margin.right,
-        height = 1000 - margin.top - margin.bottom;
+lineGraph = function (data, svg) {
 
     // append the svg object to the body of the page
-    var svg = d3.select("#SVG_CONTAINER")
-        .append("svg")
-        // .attr("width", width + margin.left + margin.right)
-        // .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform",
-            "translate(" + margin.left + "," + margin.top + ")");
-
+    chart = svg.append('g')
+        .attr("class", "lineGraph")
 
     // List of groups = header of the csv files
-    console.log(data);
     let attributes = data.columns.slice(8, 11);
 
-    // Add X axis
-    var x = d3.scaleLinear()
+    //** SCALES *****************************************
+    // X scale
+    xScale = d3.scaleLinear()
         .domain(d3.extent(data, function (d) {
             return d.year;
         }))
-        .range([0, width]);
-    svg.append("g")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x).ticks(10).tickFormat(d3.format("d")));
+        .range([MARGIN.LEFT, width - MARGIN.RIGHT]);
 
-    // Add Y axis
-    var y = d3.scaleLinear()
+    // Y scale
+    yScale = d3.scaleLinear()
         .domain([0, 3])
-        .range([height, 0]);
-    svg.append("g")
-        .call(d3.axisLeft(y));
+        .range([height - MARGIN.BOTTOM, MARGIN.TOP]);
 
-    // color palette
+    //Colour Scale for each attribute
     let color = d3.scaleOrdinal()
         .domain(attributes)
         .range(["green", "blue", "red"]);
+
+    //** CREATE AXIS *****************************************
+    let xAxis = d3.axisBottom()
+        .scale(xScale)
+        .ticks(10)
+        .tickFormat(d3.format("d"))
+
+    let yAxis = d3.axisLeft().scale(yScale);
+
+    chart.append("g")
+        .attr("class", "xAxis")
+        .attr("transform", "translate("+ 0 + ","+ (height-MARGIN.BOTTOM) +")")
+        .call(xAxis);
+
+    chart.append("g")
+        .attr("class", "yAxis")
+        .attr("transform", "translate("+ MARGIN.LEFT + "," + 0 +")")
+        .call(yAxis);
 
     //Create x and y axis labels
     let xLabel = "Year";
     let yLabel = "Attribute Measurement"
 
-    svg.append("text")
+    chart.append("text")
         .attr("class", "x_label")
         .attr("text-anchor", "end")
         .attr("x", width / 2)
@@ -83,7 +88,7 @@ lineGraph = function (data) {
         .attr("font-size", "15px")
         .text(xLabel);
 
-    svg.append("text")
+    chart.append("text")
         .attr("class", "y_label")
         .attr("text-anchor", "end")
         .attr("transform", "rotate(-90 " + (MARGIN.LEFT - 40) + " " + (height / 2 - MARGIN.TOP) + ")")
@@ -91,11 +96,14 @@ lineGraph = function (data) {
         .attr("y", height / 2 - MARGIN.LEFT * 2)
         .text(yLabel);
 
+    //** CREATE LEGEND *****************************************
     let xLegend = width;
     let yLegend = MARGIN.TOP;
 
-    //Colour encoding
-    svg.append("text")
+    legend = svg.append("g").attr("class", "legend");
+
+    //Colour encoding legend
+    legend.append("text")
         .attr("x", xLegend)
         .attr("y", yLegend - 30)
         .attr("font-weight", "bold")
@@ -103,12 +111,12 @@ lineGraph = function (data) {
 
     for(let i = 0; i < attributes.length; i++)
     {
-        svg.append("circle")
+        legend.append("circle")
             .attr("cx", xLegend)
             .attr("cy", yLegend)
             .attr("r", 6)
             .style("fill", color(attributes[i]));
-        svg.append("text")
+        legend.append("text")
             .attr("x", xLegend + 20)
             .attr("y", yLegend)
             .style("font-size", "10px")
@@ -117,6 +125,7 @@ lineGraph = function (data) {
         yLegend += 20;
     }
 
+    //** DATA POINTS *****************************************
     //Calculate averages
     let attributeSeries = d3.group(data, d => d.year);
     let groupByYearArray = Array.from(attributeSeries);
@@ -145,15 +154,16 @@ lineGraph = function (data) {
     console.log("This is the stack result: ", stackedData);
 
     // Show the areas
-    svg
-    .selectAll("mylayers")
+    finishedStack = chart.selectAll("stack")
     .data(stackedData)
     .enter()
     .append("path")
     .style("fill", function(d) { return color(attributes[d.key-2]); })
     .attr("d", d3.area()
-    .x(function(d, i) { return x(d.data[0]); })
-    .y0(function(d) { return y(d[0]); })
-    .y1(function(d) { return y(d[1]); })
-    );
+    .x(function(d, i) { return xScale(d.data[0]); })
+    .y0(function(d) { return yScale(d[0]); })
+    .y1(function(d) { return yScale(d[1]); })
+    )
+    .attr("class", d => "stack " + attributes[d.key - 2]);
+
 };
